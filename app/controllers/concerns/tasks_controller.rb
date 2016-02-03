39c37269369119ad16be5f3feece5dedbf085b6b
Ilja Goushcha <iljagou@yahoo.com>
2016-02-02 20:21:56 -0500

    setting up tasks migration, controller, and model

---
+++ app/controllers/concerns/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+
+  def index
+    @students = Student.all
+    render json: @students
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    if @student
+      render json: @student, status: :created, location: @student
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @student = Student.new(allowed_params)
+
+    if @student.save
+      render json: @student, status: :created, location: @student
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(allowed_params)
+      render json: @student, status: :created, location: @student
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+
+    if @student.destroy
+      render json: @student, status: :created, location: @student
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def allowed_params
+    params.require(:student).permit(:first_name, :last_name, :gender, :date_of_birth)
+  end
+
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    if @task
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @task = Task.new(allowed_params)
+
+    if @task.save
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(allowed_params)
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    if @task.destroy
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def allowed_params
+    params.require(:task).permit(:name, :type, :max_points, :weight)
+  end
+
+end
